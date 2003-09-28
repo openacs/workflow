14353ac02cdd72c180011adaa9bc6f4453335702
Lars Pind <lars@pinds.com>
2003-09-28 01:24:27 +0000

    Added view workflow_case_assigned_actions

--- sql/oracle/workflow-tables-create.sql
+++ sql/oracle/workflow-tables-create.sql
@@ -1,4 +1,4 @@
--- Data model for the workflow package, part of the OpenACS system.
+>-- Data model for the workflow package, part of the OpenACS system.
 --
 -- @author Lars Pind (lars@collaboraid.biz)
 -- @author Peter Marklund (peter@collaboraid.biz)
@@ -514,3 +514,24 @@ begin
 end;
 /
 show errors
+
+-----------------
+-- Useful views
+-----------------
+
+create or replace view workflow_case_assigned_actions as 
+    select c.workflow_id,
+           c.case_id, 
+           c.object_id,
+           a.action_id, 
+           a.assigned_role as role_id
+    from   workflow_cases c,
+           workflow_case_fsm cfsm,
+           workflow_actions a,
+           workflow_fsm_action_en_in_st aeis
+    where  cfsm.case_id = c.case_id
+    and    a.always_enabled_p = 'f'
+    and    aeis.state_id = cfsm.current_state
+    and    aeis.assigned_p = 't'
+    and    a.action_id = aeis.action_id
+    and    a.assigned_role is not null;
--- sql/postgresql/workflow-tables-create.sql
+++ sql/postgresql/workflow-tables-create.sql
@@ -528,3 +528,24 @@ select content_type__create_type (
   'entry_rev_id',                    -- id_column
   null                               -- name_method
 );
+
+-----------------
+-- Useful views
+-----------------
+
+create or replace view workflow_case_assigned_actions as 
+    select c.workflow_id,
+           c.case_id, 
+           c.object_id,
+           a.action_id, 
+           a.assigned_role as role_id
+    from   workflow_cases c,
+           workflow_case_fsm cfsm,
+           workflow_actions a,
+           workflow_fsm_action_en_in_st aeis
+    where  cfsm.case_id = c.case_id
+    and    a.always_enabled_p = 'f'
+    and    aeis.state_id = cfsm.current_state
+    and    aeis.assigned_p = 't'
+    and    a.action_id = aeis.action_id
+    and    a.assigned_role is not null;
